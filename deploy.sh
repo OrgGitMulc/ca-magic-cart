168310da633d57b20031272c1bacfa4d6888fd65
GitMulC <cmulvo@gmail.com>
2024-08-08 00:44:36 +0100

    Adjust deploy.sh to fix existing container error

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ CURRENT_INSTANCE=${sudo docker ps -a -q --filter ancestor="$IMAGE_NAME" --format
 # If an instance does exist stop the instance
 if [ "$CURRENT_INSTANCE" ]
 then
+    echo "Stopping current instance"
     sudo docker rm $(sudo docker stop $CURRENT_INSTANCE)
 fi
 
@@ -13,9 +14,15 @@ sudo docker pull $IMAGE_NAME
 
 # Check if a docker container exists with the name of $CONTAINER_NAME if it does remove the container
 CONTAINER_EXISTS=$(sudo docker ps -a | grep $CONTAINER_NAME)
+echo "Container Exists : " $CONTAINER_EXISTS
 if [ "$CONTAINER_EXISTS" ]
 then
+    echo "Remove existing container"
+	sudo docker stop $CONTAINER_NAME
     sudo docker rm $CONTAINER_NAME
 fi
 
 sudo docker run -p 3000:3000 -d --name $CONTAINER_NAME $IMAGE_NAME
+
+#check that the container is actually up
+sudo docker ps -a | grep $CONTAINER_NAME
