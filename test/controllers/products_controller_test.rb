e72bb732407e2553f6857b1a1d92fd13637cb063
GitMulC <cmulvo@gmail.com>
2024-08-08 00:27:58 +0100

    Fix fixture issue with product controller tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @product = products(:one)
+    @product = products(:single)
   end
 
   test "should get index" do
